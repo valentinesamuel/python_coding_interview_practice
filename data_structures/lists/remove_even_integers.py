4978216429dc5b1d22a299a24c500bf981c17731
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-15 20:35:20 +0100

    FEAT: solved remove even

---
+++ data_structures/lists/remove_even_integers.py
@@ -0,0 +1,20 @@
+
+def remove_even(lst):
+
+    """
+    for every item in the list, check the remainder using 2. If the remainder is not 0, then that number is an odd
+    number because 2 is an even number and ite can divide 
+    any even number without remainder
+    """
+
+    result = []
+    for idx in range(len(lst)):
+        # print(lst[idx])
+        if lst[idx] % 2 != 0:
+            result.append(lst[idx])
+    return(result)
+
+lst = [20, 36, -139, 12, -31, 168, -66, -36, 151, -132, 30, -17, -179, 133, -91, 61, -112, 23, 77, 53, -64, 81, 191, 8, -40, 163, -107, 29, -56, -121, -63, 45]
+
+print(remove_even(lst), end='\n 👇\n')
+print([-139, -31, 151, -17, -179, 133, -91, 61, 23, 77, 53, 81, 191, 163, -107, 29, -121, -63, 45])
