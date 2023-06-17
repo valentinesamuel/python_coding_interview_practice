bfeb49f968f2ad25f85457c9d558d528135af405
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-17 18:41:07 +0100

    FEAT: Added test cases for remove_even

--- data_structures/lists/remove_even_integers.py
+++ data_structures/lists/remove_even_integers.py
@@ -24,7 +24,44 @@ def remove_even(lst):
             result.append(lst[idx])
     return(result)
 
-lst = [20, 36, -139, 12, -31, 168, -66, -36, 151, -132, 30, -17, -179, 133, -91, 61, -112, 23, 77, 53, -64, 81, 191, 8, -40, 163, -107, 29, -56, -121, -63, 45]
 
-print(remove_even(lst), end='\n 👇\n')
-print([-139, -31, 151, -17, -179, 133, -91, 61, 23, 77, 53, 81, 191, 163, -107, 29, -121, -63, 45])
+def test_remove_even():
+    # Test case 1: Normal input
+    lst = [20, 36, -139, 12, -31, 168, -66, -36, 151, -132, 30, -17, -179, 133, -91, 61, -112, 23, 77, 53, -64, 81, 191, 8, -40, 163, -107, 29, -56, -121, -63, 45]
+    expected = [-139, -31, 151, -17, -179, 133, -91, 61, 23, 77, 53, 81, 191, 163, -107, 29, -121, -63, 45]
+    assert remove_even(lst) == expected
+
+    # Test case 2: Empty list
+    lst = []
+    expected = []
+    assert remove_even(lst) == expected
+
+    # Test case 3: List with only even numbers
+    lst = [2, 4, 6, 8, 10]
+    expected = []
+    assert remove_even(lst) == expected
+
+    # Test case 4: List with only odd numbers
+    lst = [1, 3, 5, 7, 9]
+    expected = [1, 3, 5, 7, 9]
+    assert remove_even(lst) == expected
+
+    # Test case 5: List with a single even number
+    lst = [2]
+    expected = []
+    assert remove_even(lst) == expected
+
+    # Test case 6: List with a single odd number
+    lst = [3]
+    expected = [3]
+    assert remove_even(lst) == expected
+
+    # Test case 7: List with a mix of even and odd numbers
+    lst = [2, 5, 4, 7, 6, 9, 8]
+    expected = [5, 7, 9]
+    assert remove_even(lst) == expected
+
+    print("All test cases passed!")
+
+# Run the test cases
+test_remove_even()
