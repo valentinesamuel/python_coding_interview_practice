a8f5da49383b672f5fc8759fe71e984e9960c9a1
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-26 19:54:32 +0100

    FEAT: get head node method added

---
+++ data_structures/02-linked_lists/01-insertion_at_tail.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next_element = None
+
+class LinkedList:
+    def __init__(self):
+        self.head_node = None
+
+    def get_head(self):
+        return self.head_node
+
