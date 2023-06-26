3c1134b1ad565888ebad3cfa12c371e5edad1d00
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-26 19:57:18 +0100

    FEAT: added tail insertion documentation and test cases for the linked list function

--- data_structures/02-linked_lists/01-insertion_at_tail.py
+++ data_structures/02-linked_lists/01-insertion_at_tail.py
@@ -28,6 +28,28 @@ class LinkedList:
         return True
 
     def insert_at_tail(self, value):
+        """
+        PROBLEM: We need to insert a new object at the end of the linked list. You can naturally guess, that this newly added node will point to None as it is at the tail.
+
+        Input: A linked list and an integer value.
+
+        Output: The updated linked list with the value inserted.
+
+        Example Input: 
+        Linked List = 0->1->2
+        integer = 3
+
+        Example Output:
+        Linked List = 0->1->2->3
+
+        APPROACH:
+        - Initial thoughts: We shold look for a way to add the node at the first place without losing pointers
+        
+        - Plan: 
+            - check if the linked list is empty. If it is, assign the node to the be the head node of the linked list
+            - if the list is not empty, get the head pointer of the list and iterate till the current node's pointer is None.
+            - On every iteration, assign the next element of the current node to the current node
+        """
 
         new_node = Node(value)
         if self.get_head() is None:
@@ -39,3 +61,47 @@ class LinkedList:
         current.next_element = new_node
         return
     
+def test_insert_at_tail():
+    # Test Case 1: Inserting into an empty list
+    linked_list = LinkedList()
+    linked_list.insert_at_tail(1)
+    assert linked_list.print_list() == True
+
+    # Test Case 2: Inserting into a non-empty list
+    linked_list = LinkedList()
+    linked_list.insert_at_tail(1)
+    linked_list.insert_at_tail(2)
+    linked_list.insert_at_tail(3)
+    assert linked_list.print_list() == True
+
+    # Test Case 3: Inserting multiple values into an empty list
+    linked_list = LinkedList()
+    linked_list.insert_at_tail(1)
+    linked_list.insert_at_tail(2)
+    linked_list.insert_at_tail(3)
+    assert linked_list.print_list() == True
+
+    # Test Case 4: Inserting multiple values into a non-empty list
+    linked_list = LinkedList()
+    linked_list.insert_at_tail(1)
+    linked_list.insert_at_tail(2)
+    linked_list.insert_at_tail(3)
+    linked_list.insert_at_tail(4)
+    assert linked_list.print_list() == True
+
+    # Test Case 5: Inserting a value at the tail of a large list
+    linked_list = LinkedList()
+    for i in range(1, 10):
+        linked_list.insert_at_tail(i)
+    assert linked_list.print_list() == True
+
+    # Test Case 6: Inserting a negative value at the tail
+    linked_list = LinkedList()
+    linked_list.insert_at_tail(-1)
+    linked_list.insert_at_tail(-2)
+    linked_list.insert_at_tail(-3)
+    assert linked_list.print_list() == True
+
+    print("\n\n💯💯All test cases passed!💯💯\n\n")
+
+test_insert_at_tail()
