1d76bf99a33e94cb23ce4e11071b2904ab43b48f
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-27 21:00:23 +0100

    FEAT: Added docs fir get the length of list

--- data_structures/02-linked_lists/01-insertion_at_tail.py
+++ data_structures/02-linked_lists/01-insertion_at_tail.py
@@ -29,6 +29,27 @@ class LinkedList:
         return True
 
     def length(self):
+        """
+        PROBLEM: In this problem, you have to implement the length() function which will find the length of a given linked list.
+
+        Input: A linked list.
+
+        Output: The number of nodes in the list.
+
+        Example Input: 
+        Linked List = 5->90->10->4  
+
+        Example Output:
+        4
+
+        APPROACH:
+        - Initial thoughts: Treat this like an array
+
+        - Plan: 
+            - if the head is None, that means the length is 0
+            - Get the head node, while moving through the linked list, increment the length
+            - after the the loop, increment the length by one again becasue the last node was the tail node that did not get checked because its next element was none
+        """
         len = 0
         if self.get_head():
             return len
