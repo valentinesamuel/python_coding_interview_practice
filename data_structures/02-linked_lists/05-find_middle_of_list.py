14a705f886a94a257cbf75747e61988d716b4393
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-28 11:34:48 +0100

    FEAT: Added finding the middle of a linked list

---
+++ data_structures/02-linked_lists/05-find_middle_of_list.py
@@ -0,0 +1,17 @@
+from LinkedListClass import LinkedList as linked_list
+
+def find_mid(lst):
+    if not lst.get_head():
+        return
+    trav = 0
+    length = lst.length()
+    if length % 2 == 0:
+        trav = length // 2
+    else:
+        trav = (length // 2) + 1
+    i = 1
+    curr = lst.get_head()
+    while i < trav:
+        curr = curr.next_element
+        i+=1
+    return curr.data
