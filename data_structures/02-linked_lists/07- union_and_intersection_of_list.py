b61df71a5a7ad169436f404980bbda6d0671c35b
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-06-30 14:10:49 +0100

    FEAT: Added docs for intersection

--- data_structures/02-linked_lists/07- union_and_intersection_of_list.py
+++ data_structures/02-linked_lists/07- union_and_intersection_of_list.py
@@ -9,7 +9,7 @@ def union(list1, list2):
 
     Input: Two linked lists.
 
-    Output: A list containing the union of the two lists. A list containing the intersection of the two lists.
+    Output: A list containing the union of the two lists.
 
     Example Input: 
     list1 = 10->20->80->60
@@ -17,10 +17,9 @@ def union(list1, list2):
 
     Example Output:
     union = 10->20->80->60->15->30->45
-    intersection = 20->60
 
     APPROACH:
-    - Initial thoughts: We can link the head of the second list to the tail of the first list and remove duplicates. And for the intersection, we can use a hashmap to find the previously encountered values
+    - Initial thoughts: We can link the head of the second list to the tail of the first list and remove duplicates.
 
     - Plan: 
         - iterate to the end of list 1
@@ -38,3 +37,49 @@ def union(list1, list2):
     list1.remove_duplicates()
 
     return list1
+
+def intersection(list1, list2):
+    """
+    PROBLEM: Given two lists, A and B, the intersection is the largest list which contains all the elements that are common to both the sets.
+    
+    The intersection function will return all the elements that are common between two linked lists.
+
+    Input: Two linked lists.
+
+    Output: A list containing the intersection of the two lists.
+
+    Example Input: 
+    list1 = 10->20->80->60
+    list2 = 15->20->30->60->45
+
+    Example Output:
+    intersection = 20->60
+
+    APPROACH:
+    - Initial thoughts: we can use a hashmap to find the previously encountered values
+
+    - Plan: 
+        - load all the elements of list1 into the hasmap and check each element of list2 against the hashmap.
+        - create a new linked list to return
+        - if it exists, then it is an intersectory node
+        OR
+        - we simply list1 and search for all elements in list2.
+        - if any of these values are found in list2, it is added to the result linked list. This is less effective
+    """
+    if list1.get_head is None and list1 is None:
+        return
+
+    register = {}
+    curr = list1.get_head()
+    while curr:
+        register[curr.data] = 1
+        curr = curr.next_element
+    
+    res = linked_list()
+
+    curr = list2.get_head()
+    while curr:
+        if curr.data in register:
+            res.insert_at_tail(curr.data)
+        curr = curr.next_element
+    return res
