7d6f30d804098e08d2a7798bce4edcd29e500197
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-07-05 14:06:43 +0100

    FEAT: Added docs for reversing k elements in queue

--- data_structures/03-stacks_and_queues/03-reverse_k_elements.py
+++ data_structures/03-stacks_and_queues/03-reverse_k_elements.py
@@ -2,6 +2,27 @@ from QueueClass import MyQueue
 from StackClass import MyStack as Stack
 
 def reverseK(queue, k):
+    """
+    PROBLEM: 
+
+    Input: 
+
+    Output: 
+
+    Example Input: 
+    n = 3
+
+    Example Output:
+    result = ["1","10","11"]
+
+    APPROACH:
+    - Initial thoughts: I tried but i could not solve it before my trial time of 30 mins elapsed
+
+    - Plan: 
+        - using an auxiliary stack, remove the first k elements from the queue and add it to the stack
+        - enqueue all the elements by popping them from the stack
+        - starting from  queue_length - k, dequeue all elements and enqueue them
+    """
     if k > queue.size() or k < 0 or queue.is_empty():
         return None
 
