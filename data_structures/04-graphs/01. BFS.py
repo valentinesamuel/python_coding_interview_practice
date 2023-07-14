f8d00eb1777cd2bba3bd1a1150a94391f1215d4d
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-07-14 13:26:03 +0100

    FEAT: Added bfs code

--- data_structures/04-graphs/01. BFS.py
+++ data_structures/04-graphs/01. BFS.py
@@ -4,5 +4,20 @@ import sys
 sys.path.append('../DS_Classes/')
 
 
-def bfs_traversal(g, source):
-    pass
+def bfs_traversal(g: Graph, source):
+    result = ""
+    num_of_vertices = g.vertices
+
+    queue = Queue()
+    queue.enqueue(source)
+
+    while not queue.is_empty():
+        curr = queue.dequeue()
+        result = result + str(curr)
+        head_node = g.array[curr].get_head()
+
+        while head_node:
+            queue.enqueue(head_node.data)
+            head_node = head_node.next_element
+
+    return result
