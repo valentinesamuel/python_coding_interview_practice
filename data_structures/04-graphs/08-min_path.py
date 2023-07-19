6d6836775795d6fa5bafc251485b97c1f930c9e7
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-07-19 16:44:23 +0100

    FEAT: Added find_min docs

--- data_structures/04-graphs/08-min_path.py
+++ data_structures/04-graphs/08-min_path.py
@@ -5,6 +5,36 @@ sys.path.append('../DS_Classes/')
 
 
 def find_min(graph: Graph, source: int, destination: int):
+    """
+    PROBLEM: Implement the find_min() function which will take a directed graph and two vertices, A and B. The result will be the shortest path from A to B. Remember, the shortest path will contain the minimum number of edges.
+
+    Input: A directed graph, a vertex A and a vertex B.
+
+    Output: Returns number of edges in the shortest path between A and B.
+
+    Example Input:
+    >>Adjacency List of Directed Graph<<
+    | 0 | => None
+    | 1 | => [ 3 ] -> [ 2 ] -> None
+    | 2 | => [ 5 ] -> [ 4 ] -> None
+    | 3 | => None
+    | 4 | => [ 5 ] -> None
+    | 5 | => None
+
+    Example Output:
+    2
+
+    APPROACH:
+    - Initial thoughts: I thought about doing a normal traversal and keeping track of a minimum sum and running minimum sum
+
+    - Plan: 
+        - keeep track of the node's distance from the source node as well as an array of visited and non-visited nodes.
+        - while the BFS is on going, if the node has not been visited,
+            - add it to queue
+            - mark it as visited,
+            - set the distance to be the distance od the popped node + 1
+            - if the node is the destination, return the distance array indexed at the destination
+    """
     num_vertices = graph.vertices
     visited = [False] * graph.vertices
     distance = [0] * num_vertices
