8f525e1dc5b66ca6013d82d9e8d284ffac78d99a
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-08-05 18:51:13 +0100

    FEAT: Added docs min value exercise

--- data_structures/05-trees/01-min_value.py
+++ data_structures/05-trees/01-min_value.py
@@ -5,7 +5,32 @@ sys.path.append('../DS_Classes/')
 
 
 def findMin(root: TreeNode):
+    """
+    PROBLEM: Implement the findMin(root) function which will find the minimum value in a given Binary Search Tree. Remember, a Binary Search Tree is a Binary Tree which satisfies the following property. An illustration is also provided to jog your memory.
 
+    Input: A binary search tree.
+
+    Output: Returns minimum integer value from a given binary search tree
+    Example Input:
+    bst = {
+    6 -> 4,9
+    4 -> 2,5
+    9 -> 8,12
+    12 -> 10,14
+    }
+    where parent -> leftChild,rightChild
+
+    Example Output:
+    2
+
+    APPROACH:
+    - Initial thoughts: I figured that no need to do a bfs or dfs since all the nodes in the subtree of the current node is less than the node, that means we would always traverse left
+
+    - Plan: 
+        - while there is still a left child,
+        - move to the left
+        - return the value at the end
+    """
     
     if (root is None):
         return None
