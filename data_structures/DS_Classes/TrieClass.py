57d5c353aca6510b793038af5a84bb700fa28997
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-08-12 16:04:27 +0100

    feat: added docs for trie class

--- data_structures/DS_Classes/TrieClass.py
+++ data_structures/DS_Classes/TrieClass.py
@@ -9,6 +9,10 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, word:str):
+        """insert a new word into the trie:\n
+        for every char in the word, if the char not
+        children of the current node, then create a trienode for the char, if it is the children, mark the end of the word as true
+        """
         curr = self.root
         
         for c in word:
@@ -18,21 +22,35 @@ class Trie:
         curr.isEndOfWord = True
 
     def search(self, word:str):
+        """search for a word in the trie:\n
+        for every char in the word, if the char not
+        children of the current node, return false, otherwise, move to the next trienode and
+        finally return the value os the end-of-word-ness
+
+        Args:
+            val (str|int): integer or string
+        """
         curr = self.root
 
         for c in word:
             if c not in curr.children:
                 return False
-            curr = curr.children
+            curr = curr.children[c]
         return curr.isEndOfWord
     
     def startsWith(self, prefix:str):
+        """
+        for every char in the word, if the
+        children char of the current node
+        does not contain the char return
+        false, else return true
+        """
         curr = self.root
 
         for c in prefix:
             if c not in curr.children:
                 return False
-            curr.children[c]
+            curr = curr.children[c]
         return True
         
             
