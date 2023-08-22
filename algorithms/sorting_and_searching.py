f43ad164713fd363732219d549a5b298a38078ac
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-08-22 12:24:18 +0100

    feat: added docs for section sort

--- algorithms/sorting_and_searching.py
+++ algorithms/sorting_and_searching.py
@@ -1,13 +1,20 @@
-def selection_sort(arr):
+def selection_sort(arr: list):
+    """
+    param (lst): List of integers
+    """
     for i in range(len(arr)):
+       # find the smallest element in unstorted list
         min = i
         for j in range(i+1, len(arr)):
+        # find any other smallest element in the unstorted list
             if arr[j] < arr[min]:
                 min = j
+        # if there is, perform a swap
         arr[min], arr[i] = arr[i], arr[min]
 
-lst = [3, 2, 1, 5, 4]
-selection_sort(lst)  # Calling selection sort function
 
-# Printing Sorted lst
+lst = [3, 2, 1, 5, 4]
+selection_sort(lst)
+
+
 print("Sorted lst: ", lst)
