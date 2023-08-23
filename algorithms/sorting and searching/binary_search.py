4b912d931e1a2381d3778fd72bf0b5aad099146d
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-08-23 11:53:45 +0100

    added pivoted_bs and its docs

--- algorithms/sorting and searching/binary_search.py
+++ algorithms/sorting and searching/binary_search.py
@@ -4,7 +4,7 @@ def binary_search(lst, n):
     found = False
 
     while left <= right and not found:
-        mid =  (left + right) // 2
+        mid = (left + right) // 2
         if lst[mid] == n:
             found = True
         else:
@@ -13,3 +13,41 @@ def binary_search(lst, n):
             if lst[mid] < n:
                 left = mid + 1
     return found
+
+
+def pivoted_binary_search(lst, target):
+    l = 0
+    r = len(lst)-1
+
+    while l <= r:
+        mid = (l+r) // 2
+        if target == lst[mid]:
+            return mid
+
+        # left sorted part of the input array
+        if lst[l] <= lst[mid]:
+            # if the target is between the left most and
+            # mid position of the left part of the input array
+            if target < lst[mid] and target >= lst[l]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        # right sorted part of the input array
+        else:
+            # if the target is between the right most and
+            # mid position of the right part of the input array
+            if target > lst[mid] and target <= lst[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return -1
+
+
+nums = [8, 11, 13, 15, 1, 4, 6]
+target = 1
+index = pivoted_binary_search(nums, target)
+
+if index != -1:
+    print(f"Element {target} found at index {index}.")
+else:
+    print("Element not found.")
