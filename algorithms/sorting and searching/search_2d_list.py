c401db682bcedf3c6f3d93033e369f88cca73ce6
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-08-24 10:33:00 +0100

    feat: add my personal solution for 2d array

--- algorithms/sorting and searching/search_2d_list.py
+++ algorithms/sorting and searching/search_2d_list.py
@@ -16,3 +16,28 @@ def find_in(lst: list, number: int):
             right = midpoint - 1
 
     return True
+
+
+def find_in2(lst: list, number: int):
+    left = 0
+    right = len(lst) - 1
+
+    while left <= right:
+        mid = (left+right) // 2
+        if number in lst[mid]:
+            return True
+        elif number > lst[mid][0] and number < lst[mid][-1]:
+            return number in lst[mid]
+        if number > lst[mid][-1]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+
+
+print(find_in2([[10, 11, 12, 13], [14, 15, 16, 17],
+      [27, 29, 30, 31], [32, 33, 39, 80]], 10))
+print(find_in2([[10, 11, 12, 13], [14, 15, 16, 17],
+      [27, 29, 30, 31], [32, 33, 39, 80]], 10))
+print()
